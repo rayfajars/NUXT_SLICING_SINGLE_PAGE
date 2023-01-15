7eef27a5000f660a72866a28fa29b83ba8a182f5
Ray Fajar S <rayfajars.com>
2023-01-16 05:37:55 +0700

    move navbar and footer to layouts

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <template>
   <div>
-    <NavbarSection />
     <HeroSection />
     <SelectWorkSpace />
     <InfoSection />
     <ListCoworkingSection />
-    <FooterSection />
   </div>
 </template>
 
 <script>
-import NavbarSection from '~/components/NavbarSection.vue'
 import ListCoworkingSection from '~/components/ListCoworkingSection.vue'
-import FooterSection from '~/components/FooterSection.vue'
 import InfoSection from '~/components/InfoSection.vue'
 import SelectWorkSpace from '~/components/SelectWorkSpace.vue'
 import HeroSection from '~/components/HeroSection.vue'
@@ -20,12 +16,10 @@ import HeroSection from '~/components/HeroSection.vue'
 export default {
   name: 'IndexPage',
   components: {
-    NavbarSection,
     ListCoworkingSection,
-    FooterSection,
     InfoSection,
     SelectWorkSpace,
-    HeroSection
-},
+    HeroSection,
+  },
 }
 </script>
